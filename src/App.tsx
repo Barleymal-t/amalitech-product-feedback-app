8a9b223cd507c3bf24a8f911cc63fe39fbb8cd79
Nii <niiodartey10@gmail.com>
2023-07-21 18:35:31 +0100

    Persist data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store, { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 import SuggestionsPage from "./pages/SuggestionsPage";
-import SuggestionDetailsPage from './pages/SuggestionDetailsPage';
-import NewSuggestionPage from './pages/NewSuggestionPage';
-import EditSuggestionPage from './pages/EditSuggestionPage';
-import { Provider } from 'react-redux';
-import store from "./store/store"
-import Roadmap from './pages/RoadmapPage';
-import { Cover } from './pages/page_styles';
-
+import SuggestionDetailsPage from "./pages/SuggestionDetailsPage";
+import NewSuggestionPage from "./pages/NewSuggestionPage";
+import EditSuggestionPage from "./pages/EditSuggestionPage";
+import Roadmap from "./pages/RoadmapPage";
+import { Cover } from "./pages/page_styles";
 
 function App() {
   return (
     <Cover>
-
-      
-    <Provider store={store}>
-
-    <BrowserRouter>
-    <Routes>
-      <Route index element={<SuggestionsPage/>}/>
-      <Route path="/suggestion/:id" element={<SuggestionDetailsPage/>}/>
-        <Route path="/suggestion/new" element={<NewSuggestionPage />} />
-        <Route path="/suggestion/:id/edit" element={<EditSuggestionPage />} />
-        <Route path="/roadmap" element={<Roadmap />} />
-
-
-
-    </Routes>
-    </BrowserRouter>
-    </Provider>
-  </Cover>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<SuggestionsPage />} />
+              <Route
+                path="/suggestion/:id"
+                element={<SuggestionDetailsPage />}
+              />
+              <Route path="/suggestion/new" element={<NewSuggestionPage />} />
+              <Route
+                path="/suggestion/:id/edit"
+                element={<EditSuggestionPage />}
+              />
+              <Route path="/roadmap" element={<Roadmap />} />
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </Cover>
   );
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import suggestionReducer from './suggestionsSlice'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import suggestionReducer from "./suggestionsSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const rootReducer = combineReducers({
+  request: suggestionReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    
-  reducer: {
-    request:suggestionReducer
-},
-})
+  reducer: persistedReducer,
+  middleware: [thunk],
+});
 
-export default store
+export default store;
+export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
